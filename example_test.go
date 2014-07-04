9c3b1729dce95995b71d7c8b340a7a34dbc93298
MakeNowJust <make.just.on@gmail.com>
2014-07-04 22:56:56 +0900

    Add more examples

--- example_test.go
+++ example_test.go
@@ -10,7 +10,7 @@ import (
 
 import "github.com/MakeNowJust/heredoc"
 
-func ExampleDoc() {
+func ExampleDoc_lipsum() {
 	fmt.Print(heredoc.Doc(`
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 		sed do eiusmod tempor incididunt ut labore et dolore magna
@@ -21,4 +21,26 @@ func ExampleDoc() {
 	// sed do eiusmod tempor incididunt ut labore et dolore magna
 	// aliqua. Ut enim ad minim veniam, ...
 	//
+}
+
+func ExampleDoc_spec() {
+	// Single line string is no change.
+	fmt.Println(heredoc.Doc(`It is single line.`))
+	// If first line is empty, heredoc.Doc removes first line.
+	fmt.Println(heredoc.Doc(`
+		It is first line.
+		It is second line.`))
+	// If last line is empty and more little length than indents,
+	// heredoc.Doc removes last line's content.
+	fmt.Println(heredoc.Doc(`
+		Next is last line.
+	`))
+	fmt.Println("Previous is last line.")
+	// Output:
+	// It is single line.
+	// It is first line.
+	// It is second line.
+	// Next is last line.
+	//
+	// Previous is last line.
 }
