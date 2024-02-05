fc53b5b5183c57dcae53ffe2a2368f02f4c75928
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-02-05 13:09:52 +0900

    Update copyright year 2023 -> 2024

--- heredoc.go
+++ heredoc.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2023 TSUYUSATO Kitsune
+// Copyright (c) 2014-2024 TSUYUSATO Kitsune
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 
